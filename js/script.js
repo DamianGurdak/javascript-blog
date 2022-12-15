90ef76353d57823ec8477c428a7dcbb241ea30ea
Damian Gurdak <damiangurdakk@gmail.com>
2022-12-15 21:07:46 +0100

    add generateTags function

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 const titleClickHandler = function (event) {
   event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!');
+  // console.log('Link was clicked!');
 
   // console.log('Wyświetl argument event: ', event); //test button
 
@@ -31,25 +31,25 @@ const titleClickHandler = function (event) {
   /* [DONE] get 'href' attribute from the clicked link */
   // zapisanq wartość atrybutu href klikniętego linka
   const articleSelector = clickedElement.getAttribute('href');
-  console.log('atrybut href linku: ', articleSelector);
+  // console.log('atrybut href linku: ', articleSelector);
 
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
   const targetArticle = document.querySelector(articleSelector);
-  console.log('artykuł: ', targetArticle);
+  // console.log('artykuł: ', targetArticle);
 
   /* [IN PRGRESS] add class 'active' to the correct article */
 
   targetArticle.classList.add('active');
 };
 
-//---------------------------------------------------------------------
+//-------------------Generowanie listy tytułów-------------------
 
 const optArticleSelector = '.post', // pojendynczy artykuł
   optTitleSelector = '.post-title', // tytuł pojeynczego artykułu
-  optTitleListSelector = '.titles'; // lista ul linków lewa kolumna
-
-const articles = document.querySelectorAll(optArticleSelector);
+  optTitleListSelector = '.titles', // lista ul linków lewa kolumna
+  articles = document.querySelectorAll(optArticleSelector),
+  optArticleTagsSelector = '.post-tags .list'; // lista ul  tagów poszczególnych artykułów
 
 function generateTitleLinks() {
   /* remove contents of titleList */
@@ -61,7 +61,7 @@ function generateTitleLinks() {
   for (let article of articles) {
     /* get the article id */
     const articleId = article.getAttribute('id');
-    console.log('article id: ', articleId);
+    // console.log('article id: ', articleId);
 
     /* find the title element */
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
@@ -73,22 +73,65 @@ function generateTitleLinks() {
       '"><span>' +
       articleTitle +
       '</span></a></li>';
-    console.log('generowany link: ', linkHTML);
+    // console.log('generowany link: ', linkHTML);
     /* insert link into titleList */
     // titleList.innerHTML = titleList.innerHTML + linkHTML; // mnirj wydajne
 
     // const linkList = document.querySelector(optTitleListSelector);
     // linkList.insertAdjacentHTML('beforeend', linkHTML);
     html = html + linkHTML;
-    console.log(html);
+    // console.log(html);
   }
   titleList.innerHTML = html;
 }
 
 generateTitleLinks();
 const links = document.querySelectorAll('.titles a');
-console.log('stała links: ', links);
+// console.log('stała links: ', links);
 
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
+
+//-------------------Przypisanie tagów do artykułów-------------------
+
+function generateTags() {
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+  // console.log('artykuły: ', articles);
+
+  /* START LOOP: for every article: */
+  for (let article of articles) {
+    /* find tags wrapper */
+    const titleList = article.querySelector(optArticleTagsSelector);
+    // console.log('tag-wrapper: ', titleList);
+
+    /* make html variable with empty string */
+    let html = '';
+
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+    // console.log('article-tag: ', articleTags);
+
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+    // console.log('tablica tagów: ', articleTagsArray)
+
+    /* START LOOP: for each tag */
+    for (let tag of articleTagsArray) {
+      // console.log('pojedynczy tag:', tag);
+      /* generate HTML of the link */
+      const linkTag = '<li><a href="#' + tag + '">' + tag + '</a></li>';
+      /* add generated code to html variable */
+      html = html + linkTag;
+
+      /* END LOOP: for each tag */
+    }
+    /* insert HTML of all the links into the tags wrapper */
+    titleList.innerHTML = html;
+    /* END LOOP: for every article: */
+  }
+  
+}
+
+generateTags();
