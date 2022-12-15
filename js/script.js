a02d98ed9801fea3e46cc61ce98c5b8a3ead5887
Damian Gurdak <damiangurdakk@gmail.com>
2022-12-15 16:15:33 +0100

    add active class to clicked link

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const titleClickHandler = function (event) {
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log('Wyświetl argument event: ', event);
 
-  /* remove class 'active' from all article links  */
+  // console.log('Wyświetl argument event: ', event); //test button
+
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,9 +14,12 @@ const titleClickHandler = function (event) {
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS]  add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts article.active');
 
